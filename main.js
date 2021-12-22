33d94b719fe181d818393aa1415ef374345749f9
Agu0311 <79716442+lol123-ab@users.noreply.github.com>
2021-12-22 16:02:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var status="";
+object=[];
+var object_name="";
 
 function setup(){
     canvas= createCanvas(380,350);
@@ -18,6 +20,43 @@ function modelLoaded(){
     status= true;
 }
 
+function gotResults(error,result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        object=result;
+    }
+}
+
 function draw(){
     image(video,0,0,380,380);
+    if(status != ""){
+        for(i=0; i<object.length; i++){
+            percent= floor(object[i].confidence*100);
+            fill("#fff800");
+            text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+            stroke("#fff800");
+            nofill();
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+    if(object.label == object_name){
+        video.stop();
+        objectDetector.detect(gotResults);
+        document.getElementById("status").innerHTML=object_name+"is found";
+        function speak(){
+            var synth = window.speechSynthesis;
+        
+            speak_data = object_name+" is found";
+        
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+        
+            synth.speak(utterThis);
+        }
+    }
+    else{
+        document.getElementById("status").innerHTML= object_name+" is not found";
+    }
 }
